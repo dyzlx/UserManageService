6a74ca427fefe4c8c62d3a73135281a430a4c71c
duyunze <13212068907@163.com>
2020-02-07 16:48:20 +0800

    remove preposition of rest api

--- userservice-api/src/main/java/com/dyz/userservice/api/interceptor/UserContextParseInterceptor.java
+++ userservice-api/src/main/java/com/dyz/userservice/api/interceptor/UserContextParseInterceptor.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Slf4j
-@WebFilter(filterName = "userContextParseFilter", urlPatterns = {"/userservice/*"})
+@WebFilter(filterName = "userContextParseFilter", urlPatterns = {"/*"})
 @Component
 public class UserContextParseInterceptor implements Filter {
 
