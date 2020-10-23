5342aa7ddd4edabfbb84e69521c9a1fde7a9a5e0
eduxyun <yunze.du@ericsson.com>
2020-10-23 14:42:21 +0800

    support graceful shutdown in spring boot 2.3 version

--- userservice-common/src/main/java/com/dyz/userservice/common/config/GracefulShutdownConfiguration.java
+++ userservice-common/src/main/java/com/dyz/userservice/common/config/GracefulShutdownConfiguration.java
@@ -1,42 +1,16 @@
 package com.dyz.userservice.common.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.connector.Connector;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextClosedEvent;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
 @Slf4j
 @Configuration
-public class GracefulShutdownConfiguration  implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
+public class GracefulShutdownConfiguration implements ApplicationListener<ContextClosedEvent> {
 
-    private volatile Connector connector;
-
-    @Override
-    public void customize(Connector connector) {
-        this.connector = connector;
-    }
     @Override
     public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
-        log.info("shutdown application!");
-        this.connector.pause();
-        Executor executor = this.connector.getProtocolHandler().getExecutor();
-        if (executor instanceof ThreadPoolExecutor) {
-            try {
-                ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
-                threadPoolExecutor.shutdown();
-                int waitTime = 30;
-                if (!threadPoolExecutor.awaitTermination(waitTime, TimeUnit.SECONDS)) {
-                    log.warn("Tomcat thread pool did not shut down gracefully within " + waitTime + " seconds. Proceeding with forceful shutdown");
-                }
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
-        }
+
     }
 }
